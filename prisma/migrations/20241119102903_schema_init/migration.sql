ec0279f407860c52fbed80e90661f9f474dc85f8
techpepson <ddpeprah@st.ug.edu.gh>
2024-11-19 10:41:58 +0000

    feat(neon workflow): add a neon workflow to the root directory

---
+++ prisma/migrations/20241119102903_schema_init/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Host" (
+    "id" SERIAL NOT NULL,
+
+    CONSTRAINT "Host_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Host_id_key" ON "Host"("id");
